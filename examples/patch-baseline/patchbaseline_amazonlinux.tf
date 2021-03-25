c9b0115d16c86a9235151faf56966d52e8740865
Jérôme PARNAUDEAU <jparnaudeau@ippon.fr>
2021-03-25 12:09:21 +0100

    fix Issue1 : AmazonLinux2018.09 does not exists

--- examples/patch-baseline/patchbaseline_amazonlinux.tf
+++ examples/patch-baseline/patchbaseline_amazonlinux.tf
@@ -26,7 +26,7 @@ module "patch_baseline_amazonlinux" {
       patch_baseline_filters = [
         {
           name   = "PRODUCT"
-          values = ["AmazonLinux2012.03", "AmazonLinux2013.03", "AmazonLinux2013.09", "AmazonLinux2014.03", "AmazonLinux2014.09", "AmazonLinux2015.03", "AmazonLinux2015.09", "AmazonLinux2016.03", "AmazonLinux2016.09", "AmazonLinux2017.03", "AmazonLinux2017.09", "AmazonLinux2018.09"]
+          values = ["AmazonLinux2012.03", "AmazonLinux2013.03", "AmazonLinux2013.09", "AmazonLinux2014.03", "AmazonLinux2014.09", "AmazonLinux2015.03", "AmazonLinux2015.09", "AmazonLinux2016.03", "AmazonLinux2016.09", "AmazonLinux2017.03", "AmazonLinux2017.09", "AmazonLinux2018.03"]
         },
         {
           name   = "CLASSIFICATION"
