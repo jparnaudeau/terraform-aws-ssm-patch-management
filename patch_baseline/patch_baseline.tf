b53b3ea3fdc77d64a794de51a9be7ad79439aadc
PARNAUDEAU <jparnaudeau@ippon.fr>
2021-06-23 11:51:57 +0200

    Use official terraform resource aws_ssm_patch_group (since bug is fixed) + fix region collision in the dergister action

--- patch_baseline/patch_baseline.tf
+++ patch_baseline/patch_baseline.tf
@@ -33,64 +33,65 @@ resource "aws_ssm_patch_baseline" "baseline" {
 # bug in terraform provider on aws_ssm_patch_group : can't associated the same patch group to a patch baseline. But it's not in accordance with the AWS Officials Docs
 # need to use a null_resource instead
 # https://github.com/terraform-providers/terraform-provider-aws/issues/9603
+# Fixed in version 0.33 of the provider
 
-# resource "aws_ssm_patch_group" "install_patchgroup" {
-#   count       = length(var.install_patch_groups)
-#   baseline_id = aws_ssm_patch_baseline.baseline.id
-#   patch_group = element(var.install_patch_groups, count.index)
-# }
-
-resource "null_resource" "register_patch_baseline_for_install_patch_group" {
-  count = length(var.install_patch_groups)
-
-  triggers = {
-    baseline_id               = aws_ssm_patch_baseline.baseline.id
-    install_patch_groups_list = join(",", var.install_patch_groups)
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      ${path.module}/register-patch-baseline-for-patch-group.sh ${self.triggers.baseline_id} ${self.triggers.install_patch_groups_list}
-EOT
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-      ${path.module}/deregister-patch-baseline-for-patch-group.sh ${self.triggers.baseline_id} ${self.triggers.install_patch_groups_list}
-EOT
-  }
+resource "aws_ssm_patch_group" "install_patchgroup" {
+  for_each = toset(var.install_patch_groups)
+    baseline_id = aws_ssm_patch_baseline.baseline.id
+    patch_group = each.key
 }
 
+# resource "null_resource" "register_patch_baseline_for_install_patch_group" {
+#   count = length(var.install_patch_groups)
 
-# resource "aws_ssm_patch_group" "scan_patchgroup" {
-#   count       = (var.enable_mode_scan ? 1 : 0) * length(var.scan_patch_groups)
-#   baseline_id = aws_ssm_patch_baseline.baseline.id
-#   patch_group = element(var.scan_patch_groups, count.index)
+#   triggers = {
+#     baseline_id               = aws_ssm_patch_baseline.baseline.id
+#     install_patch_groups_list = join(",", var.install_patch_groups)
+#   }
+
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       ${path.module}/register-patch-baseline-for-patch-group.sh ${self.triggers.baseline_id} ${self.triggers.install_patch_groups_list}
+# EOT
+#   }
+
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = <<EOT
+#       ${path.module}/deregister-patch-baseline-for-patch-group.sh ${self.triggers.baseline_id} ${self.triggers.install_patch_groups_list}
+# EOT
+#   }
 # }
+
+
+resource "aws_ssm_patch_group" "scan_patchgroup" {
+  for_each = var.enable_mode_scan ? toset(var.scan_patch_groups) : []
+    baseline_id = aws_ssm_patch_baseline.baseline.id
+    patch_group = each.key
+}
 
 # bug in terraform provider on aws_ssm_patch_group : can't associated the same patch group to a patch baseline. 
 # But it's not in accordance with the AWS Officials Docs.
 # need to use a null_resource instead
 # https://github.com/terraform-providers/terraform-provider-aws/issues/9603
-resource "null_resource" "register_patch_baseline_for_scan_patch_group" {
-  count = (var.enable_mode_scan ? 1 : 0) * length(var.scan_patch_groups)
+# resource "null_resource" "register_patch_baseline_for_scan_patch_group" {
+#   count = (var.enable_mode_scan ? 1 : 0) * length(var.scan_patch_groups)
 
-  triggers = {
-    baseline_id            = aws_ssm_patch_baseline.baseline.id
-    scan_patch_groups_list = join(",", var.scan_patch_groups)
-  }
+#   triggers = {
+#     baseline_id            = aws_ssm_patch_baseline.baseline.id
+#     scan_patch_groups_list = join(",", var.scan_patch_groups)
+#   }
 
-  provisioner "local-exec" {
-    command = <<EOT
-      ${path.module}/register-patch-baseline-for-patch-group.sh ${self.triggers.baseline_id} ${self.triggers.scan_patch_groups_list}
-EOT
-  }
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       ${path.module}/register-patch-baseline-for-patch-group.sh ${self.triggers.baseline_id} ${self.triggers.scan_patch_groups_list}
+# EOT
+#   }
 
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-      ${path.module}/deregister-patch-baseline-for-patch-group.sh ${self.triggers.baseline_id} ${self.triggers.scan_patch_groups_list}
-EOT
-  }
-}
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = <<EOT
+#       ${path.module}/deregister-patch-baseline-for-patch-group.sh ${self.triggers.baseline_id} ${self.triggers.scan_patch_groups_list}
+# EOT
+#   }
+# }
