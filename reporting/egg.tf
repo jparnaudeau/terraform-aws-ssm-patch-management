6738994d5cb802f7ef9468cdcb69afc135420869
Bevin Smith <bevinsmith@Bevins-MacBook-Pro.local>
2022-05-13 15:55:18 -0500

    pushing up succesful init

--- reporting/egg.tf
+++ reporting/egg.tf
@@ -3,8 +3,8 @@ data "aws_region" "current" {}
 data "local_file" "baselineIds" {
     filename = "${path.module}/baselineIds.json"
 }
- 
-triggers = {
+
+provider "aws" {
     region           = data.aws_region.current.name
     operating_system = var.operating_system
     baseline_id      = var.patch_baseline_id
@@ -15,8 +15,14 @@ resource "local_file" "baselineIds" {
     filename = "${path.module}/baselineIds.json"
 }
 
-provisioner "local-exec" {
+resource "null_resource" "baselineIds" {
+    provisioner "local-exec" {
     command = <<EOT
       ${path.module}/_init_.py.py register ${self.triggers.baseline_id} ${self.triggers.operating_system} ${self.triggers.region} >> "${path.module}/baselineIds"
 EOT
+  }
+}
+
+  output "baselineIds" {
+      value = local_file.baselineIds.content
   }
