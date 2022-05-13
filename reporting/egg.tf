90822e1c8b1ff4e1003e2af18146a519b15ec873
Bevin Smith <bevinsmith@Bevins-MacBook-Pro.local>
2022-05-13 14:53:57 -0500

    export data into a file object

--- reporting/egg.tf
+++ reporting/egg.tf
@@ -7,9 +7,13 @@ data "aws_region" "current" {}
     baseline_id      = var.patch_baseline_id
 }
 
+resource "local_file" "foo" {
+    content  = ""
+    filename = "${path.module}/baselineIds"
+}
 
 provisioner "local-exec" {
     command = <<EOT
-      ${path.module}/_init_.py.py register ${self.triggers.baseline_id} ${self.triggers.operating_system} ${self.triggers.region}
+      ${path.module}/_init_.py.py register ${self.triggers.baseline_id} ${self.triggers.operating_system} ${self.triggers.region} >> "${path.module}/baselineIds"
 EOT
   }
