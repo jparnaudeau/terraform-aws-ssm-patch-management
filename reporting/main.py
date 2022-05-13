a3c1af2c75bbd3c68184e74c1934e85ec57b60d8
Bevin Smith <bevinsmith@Bevins-MacBook-Pro.local>
2022-05-13 12:27:33 -0500

    adding client

--- reporting/main.py
+++ reporting/main.py
@@ -1,19 +1,26 @@
-def retreiveEffectivePatchBaselines(client,baselineId)
-response = client.describe_effective_patches_for_patch_baseline(
+ssm_config = Config(region_name = region)
+
+ssm = boto3.client('ssm',config=)
+
+def retreiveEffectivePatchBaselines(ssm,baselineId)
+response = ssm.describe_effective_patches_for_patch_baseline(
     BaselineId=baselineId,
     MaxResults=123,
     NextToken='string'
 )
 
 
-def retrieveAllPatchBaselines(client,operatingSystem):
-    # the AWS default Patch Baseline are the same in any AWS Account, depending of the region
+def retrieveAllPatchBaselines(ssm,operatingSystem):
+    # 
 
-    response = client.describe_patch_baselines()
+    response = ssm.describe_patch_baselines()
 
     baseLineIdentities = response['BaselineIdentities']
 
     for baselineIdentity in baseLineIdentities:
         baselineId=baselineIdentity['BaselineId']
-        result = retrieveEffectivePatchBaselines(client,baselineId)
+        result = retrieveEffectivePatchBaselines(ssm,baselineId)
         return result
+
+
+def main(ssm):
